f18705e5efb7bb8e23e74ef6e70024411d7f54dd
Suman <suman.mukherjee01@gmail.com>
2024-11-19 19:01:33 +0530

    add

--- ws-16nov24/hw.js
+++ ws-16nov24/hw.js
@@ -1,17 +1,17 @@
 //removing duplicates from array
-let arr = [1,1,3,5,6,7,7];
+let arr = [1, 1, 3, 5, 6, 7, 7];
 
-function removeduplicate(num){
-  return num.filter((p,i) => num.indexOf(p) === i);
+function removeduplicate(num) {
+    return num.filter((p, i) => num.indexOf(p) === i);
 }
 // removeduplicate(arr);
 // console.log(removeduplicate(arr));
 
 //flatten an array
-let numArr = [1,[2,[3,4],5],6];
+let numArr = [1, [2, [3, 4], 5], 6];
 
-function flatArr(arr){
-    return arr.reduce((acc,value)=>acc.concat(Array.isArray(value)?flatArr(value):value),[]);
+function flatArr(arr) {
+    return arr.reduce((acc, value) => acc.concat(Array.isArray(value) ? flatArr(value) : value), []);
 }
 // console.log(flatArr(numArr));
 
@@ -19,10 +19,10 @@ function flatArr(arr){
 
 let str = 'Suman';
 
-function reverseString(userName){
+function reverseString(userName) {
     let emptyStr = '';
-    for(let i = userName.length - 1; i>=0; i--){
-        emptyStr+=userName[i];
+    for (let i = userName.length - 1; i >= 0; i--) {
+        emptyStr += userName[i];
     }
     return emptyStr;
 }
@@ -31,14 +31,14 @@ function reverseString(userName){
 //Check if a string is a palindrome
 
 let user = 'inrrnia';
-function palindromeCheck(userName){
+function palindromeCheck(userName) {
     let emptyStr = '';
-    for(let i = userName.length - 1; i>=0; i--){
-        emptyStr+=userName[i];
+    for (let i = userName.length - 1; i >= 0; i--) {
+        emptyStr += userName[i];
     }
-    if(emptyStr == userName){
+    if (emptyStr == userName) {
         return 'palindrome';
-    }else{
+    } else {
         return 'not a palindrome';
     }
 }
@@ -72,11 +72,34 @@ let strn2 = 'enlist';
 const anagramCheck = (param) => param.replace(/\s/g, '').toLowerCase().split('').sort().join('');
 
 function checkAn(params1, params2, cb) {
-    if(cb(params1) === cb(params2)){
+    if (cb(params1) === cb(params2)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
-console.log(checkAn(strn1,strn2,anagramCheck));
+// console.log(checkAn(strn1,strn2,anagramCheck));
 
+//write a function to find first non-repeating letter in a string
+let val = 'sumanau';
+let finalVal = val.toLowerCase().split('');
+// console.log(finalVal);
+
+// function findRepeat(params) {
+//     let newVal;
+//     for (let i in params) {
+//         newVal[i] = (newVal[i] || 0) + 1;
+//     }
+//     return newVal;
+// }
+
+function countation(param,cb){
+    return param.forEach((v,i )=> {
+    //    console.log(v);
+       if(param.indexOf(v) === i){
+        console.log(v);
+       }
+    })
+}
+countation(finalVal);
+// console.log(countation(finalVal));
