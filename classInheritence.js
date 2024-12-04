4b6288c27485523c516e728801d2bd1738a627f1
Suman <suman.mukherjee01@gmail.com>
2024-12-04 19:13:00 +0530

    append child practice

--- classInheritence.js
+++ classInheritence.js
@@ -5,7 +5,7 @@
 // document.getElementById("row").removeEventListener("click",abc);
 
 //problem 1
-let num = [2,3,4,5,6];
+let num = [2,6,4,5,7];
 
 let newNum = num.map(v =>v*2);
 
@@ -39,5 +39,48 @@ let primeNum = num.filter(v =>{
     }
     return true;
 })
-console.log(primeNum);
+// console.log(primeNum);
 
+//6 sorting of array
+
+let sortedArr = num.sort((p,s)=>p-s);
+// console.log(sortedArr);
+
+
+//7 Removing duplicate values
+let newArr = num.filter((v,i) => num.indexOf(v) === i);
+// console.log(newArr);
+
+
+//8 sum of array elements using rest parameter
+function sumOfItems (...args) {
+    return args.reduce(function (acc,curr){
+        return acc+curr;
+    },0);
+}
+// let newNumArr = num.toString();
+// console.log(sumOfItems(2,4,5,6,7));
+// console.log(newNumArr);
+
+//9 average of numbers using rest parameter
+function avg(...params){
+        let sum = params.reduce((acc,curr)=>acc+curr,0);
+        return sum / params.length;
+}
+// console.log(avg(2,4,5,6,7));
+
+// 10 return of largest number by rest parameter
+function largestNumber(...args){
+    return args.reduce((acc,curr)=>{
+        if(acc > curr) return acc;
+        else return curr;
+    })
+}
+// console.log(largestNumber(2,4,9,6,11));
+
+
+//12 returning largeststring using rest parameter
+function largestString(...str){
+    return str.filter(v => v.length >= 4);
+}
+console.log(largestString("suman","adi","oioiweoi","dfdr"));
