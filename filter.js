ad0c35a70495c96d3cc1436c56499c53351aace8
Suman <suman.mukherjee01@gmail.com>
2024-12-19 18:58:05 +0530

    new project weather

--- filter.js
+++ filter.js
@@ -4,20 +4,50 @@
 //     // Return an empty array if no elements pass the truth test
 // }
 
-const items = [1, 2, 3, 4, 5, 6];
+const items = [1, 2, 3, 4, 5, 6, 6, 4];
 
-function match(p,s){
+function match(p, s) {
     return p === s;
 }
 
-function filter(elements, cb){
+function filter(elements, cb) {
     let arr = [];
-    for(let i=0; i<elements.length;i++){
-        if(cb(elements[i],elements[i+1]) === true ){
+    for (let i = 0; i < elements.length; i++) {
+        if (cb(elements[i], elements[i + 1]) === true) {
             arr.push(elements[i]);
         }
     }
     return arr;
 }
-console.log(filter(items,match));
+// console.log(filter(items, match));
 
+function filterArr(arr, cb) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index++) {
+        if (cb(arr[index], index,arr)) {
+            newArr.push(arr[index]);
+        }
+    }
+    return newArr;
+}
+function matchElement(p, s) {
+    return p === s;
+}
+
+// console.log("Filtered Array --> ", filterArr(items, matchElement));
+
+
+let numArr = [3,4,2,3,4,5,6];
+
+function filterNum (arr) {
+    let newArr = [];
+    for(let i = 0 ; i< arr.length ; i ++){
+        for(let j = 0 ; j < arr.length ; j ++){
+            if(i === j){
+                newArr.push(arr[j]);
+            }
+        }
+    }
+    return newArr;
+}
+console.log(filterNum(numArr));
--- misc.js
+++ misc.js
@@ -1,10 +1,10 @@
-let arr = ["df","rt","ty","ew","de"];
+let arr = ["df", "rt", "ty", "ew", "de"];
 // console.log(arr.indexOf("rt",0));  
 
 // console.log(arr.findIndex((p)=>p=="ty"));
 
 
-let user = ["Amir", "Suvas", "Dhiman", "Shankar","Pari"];
+let user = ["Amir", "Suvas", "Dhiman", "Shankar", "Pari"];
 
 // console.log(user.flatMap(p => p));
 
@@ -12,17 +12,44 @@ let user = ["Amir", "Suvas", "Dhiman", "Shankar","Pari"];
 let str = "I am a good Boy";
 
 let newStr = str.toLowerCase().split(" ").join(" ");
-let str2 = newStr.substring(0,1).toUpperCase() + newStr.substring(1) 
-console.log(str2);
+let str2 = newStr.substring(0, 1).toUpperCase() + newStr.substring(1)
+// console.log(str2);
 
-function capitalize(param){
-    return param.substring(0,1).toUpperCase() + param.substring(1);
+function capitalize(param) {
+    return param.substring(0, 1).toUpperCase() + param.substring(1);
 }
 
-function final(str,cb){
-    return str.map( param => {
+function final(str, cb) {
+    return str.map(param => {
         console.log(cb(param));
     })
-} 
+}
 // final(newStr,capitalize)
 // console.log(final(newStr,capitalize));
+
+let say = "Impossible";
+
+let newSay = say.split('');
+
+console.log(newSay.reduce(function(arr,curr){
+    arr.push(curr);
+    return arr;
+},[]));
+
+
+// console.log(newSay);
+
+function makeSentence(str) {
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        // console.log(str[i]);
+        if (str[i] == 'i' || str[i] == 'm' || str[i] == 'p' || str[i] == 'o' || str[i] == 's' || str[i] == 'b' || str[i] == 'l' || str[i] == 'e') {
+            arr.push(arr[i]);
+        }
+    }
+    return arr;
+}
+// makeSentence(newSay);
+// console.log(makeSentence(newSay));
+
+
