620dbd8dc4ff5f056e1a392019f88baa5876bc72
Suman <suman.mukherjee01@gmail.com>
2025-01-03 19:03:56 +0530

    ts

--- ts/ts-1.js
+++ ts/ts-1.js
@@ -1,4 +1,12 @@
-var num;
-num = 10;
-num = 'k';
-console.log(num);
+// 
+//Object type
+var employee;
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+var fn = employee.firstName;
+// console.log(employee);
+console.log(fn);
--- ts/ts-1.ts
+++ ts/ts-1.ts
@@ -1,5 +1,16 @@
-let num;
-num = 10;
-num = 'k';
+// 
 
-console.log(num);
+//Object type
+
+let employee: object;
+
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+let fn:object = employee.firstName;
+// console.log(employee);
+console.log();
